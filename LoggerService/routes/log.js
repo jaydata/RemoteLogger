9766570d493c03ee71f8a10b864f0eb8086f2eff
PeterAronZentai <peter.zentai@jaystack.com>
2015-02-11 20:51:56 +0100

    x

--- LoggerService/routes/log.js
+++ LoggerService/routes/log.js
@@ -5,7 +5,8 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function (req, res) {
-    res.send('respond with a resource');
+    var rc = redis.createClient(6379, "caravan-test-proxy1.cloudapp.net");
+    rc.pubsub("channels", res.json.bind(res));
 });
 
 module.exports = router;
